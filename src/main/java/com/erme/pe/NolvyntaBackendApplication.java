3217f792f3cdc6df9e7e5896912f48e441141e6f
eorbezom <eorbezo99@gmail.com>
2026-06-09 21:32:03 -0500

    se inicia con carpeta vacias conexion con pgadmin

---
+++ src/main/java/com/erme/pe/NolvyntaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.erme.pe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NolvyntaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NolvyntaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/erme/pe/NolvyntaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erme.pe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NolvyntaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
